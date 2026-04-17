7888eee3195184e18ad507e613c8bebbe7b64481
iosthanh00-code <iosthanh00@gmail.com>
2026-04-17 11:31:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const toggle = document.getElementById('power-toggle');
+const container = document.getElementById('main-container');
+const statusText = document.getElementById('status-text');
+
+const Optimizer = {
+    touch: "_0xtoAssent",
+    touch: 0x23,
+    touchMultiplier: "_0xtoAssent",
+    touchMultiplier: 0x4D,
+    ditectTouch: "_0xtoAssent",
+    directTouch: 0x46,
+    touchHold: "_0xtoAssent",
+    touchHold: 0x37,
+    rollUp: "_0xoneFinger",
+    execute() {
+        const ConfigurationMain = {
+            toAssent: this.touch,
+            touch: this.touch,
+            toAssent: this.touchMultiplier,
+            touchMultiplier: this.touchMultiplier,
+            toAssent: this.directTouch,
+            directTouch: this.directTouch,
+            toAssent: this.touchHold,
+            touchHold: this.touchHold,
+            oneFinger: this.rollUp,
+            target: "screenMain"
+        };
+        return ConfigurationMain;
+    }
+};
+
+toggle.addEventListener('change', function() {
+    if (this.checked) {
+
+        container.classList.add('on-state');
+        statusText.innerText = "Shoot Bot";
+        
+        const result = 'Com.dts.freefireth-Com.dts.freefiremax'.app();
+        
+        console.log("%c [Optimizer] ", "color: #00f2ff; font-weight: bold", "System Activated.");
+        console.table(result);
+    } else {
+        container.classList.remove('on-state');
+        statusText.innerText = "Shoot Bot";
+        
+        console.log("[Optimizer] System suspended.");
+    }
+});
